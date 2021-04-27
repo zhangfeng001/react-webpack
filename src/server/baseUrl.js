5c7ccc007d3bb915c3dc9caf7c7b7aece47f2053
zhangfeng001 <1121221073@qq.com>
2021-04-27 13:27:11 +0800

    修改api

--- src/server/baseUrl.js
+++ src/server/baseUrl.js
@@ -1,8 +1,8 @@
 export default {
     TEST_URL:{
-        BASE_API: 'https://community-test.bjbryy.cn',     // 基础服务
+        BASE_API: '',     // 基础服务
     },
     PROD_URL:{
-        BASE_API: 'https://community.bjbryy.cn',     // 基础服务
+        BASE_API: '',     // 基础服务
     }
 }
